6f827374f312d5c2ac4ebfc7e9513fd0cf45f862
kodcoin <37445601+kodcoin@users.noreply.github.com>
2018-10-24 00:28:24 +0900

    Initial commit

---
+++ core/src/main/java/org/darkcoinj/DarkSendBroadcastTransaction.java
@@ -0,0 +1,14 @@
+package org.darkcoinj;
+
+import org.kodj.core.Transaction;
+import org.kodj.core.TransactionInput;
+
+/**
+ * Created by Eric on 2/8/2015.
+ */
+public class DarkSendBroadcastTransaction {
+    Transaction tx;
+    TransactionInput vin;
+    byte [] vchSig;
+    long sigTime;
+}
---
+++ core/src/main/java/org/pivxj/kits/LevelDBWalletAppKit.java
@@ -0,0 +1,25 @@
+package org.kodj.kits;
+
+import org.kodj.core.NetworkParameters;
+import org.kodj.store.BlockStore;
+import org.kodj.store.BlockStoreException;
+import org.kodj.store.LevelDBBlockStore;
+import org.kodj.store.SPVBlockStore;
+
+import java.io.File;
+
+/**
+ * Created by Eric on 2/23/2016.
+ */
+public class LevelDBWalletAppKit extends WalletAppKit {
+    public LevelDBWalletAppKit(NetworkParameters params, File directory, String filePrefix) {
+        super(params, directory, filePrefix);
+    }
+
+    /**
+     * Override this to use a {@link BlockStore} that isn't the default of {@link SPVBlockStore}.
+     */
+    protected BlockStore provideBlockStore(File file) throws BlockStoreException {
+        return new LevelDBBlockStore(context, file);
+    }
+}
